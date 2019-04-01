2eb5d0414c4827b51ba16438fe490b4a751bfd98
administrador <administrador@DESKTOP-JDDGC.mshome.net>
2019-04-01 02:00:18 -0600

    add

---
+++ Prueba/src/paquetePrueba/Principal.java
@@ -0,0 +1,13 @@
+package paquetePrueba;
+
+import javax.swing.JOptionPane;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Hola");
+	
+	}
+
+}
