4b36e41c5423be5dd23f33361bfc0bc39076c351
administrador <administrador@DESKTOP-JDDGC.mshome.net>
2019-04-01 01:54:49 -0600

    cambio2

--- TraductorMorse/src/paqueteTraductor/PrincipalTraductor.java
+++ TraductorMorse/src/paqueteTraductor/PrincipalTraductor.java
@@ -4,15 +4,15 @@ public class PrincipalTraductor {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		/*String abcmorse[] ={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..",".----","..---","...--","....-",".....","-....","--...","---..","----.","-----"};
+		String abcmorse[] ={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..",".----","..---","...--","....-",".....","-....","--...","---..","----.","-----"};
 		String abecedario="ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
 		String texto;
 		String cadenasalida="";
 		Scanner teclado=new Scanner(System.in);
 		System.out.print("Introduzca una cadena: ");
-		texto = teclado.nextLine().toUpperCase();*/
+		texto = teclado.nextLine().toUpperCase();
 		
-		/*for (int i = 0; i < texto.length(); i++) {
+		for (int i = 0; i < texto.length(); i++) {
 			if(texto.charAt(i)==' '){
 				cadenasalida=cadenasalida+"  ";
 			}else{
@@ -21,7 +21,7 @@ public class PrincipalTraductor {
 			
 		}
 		System.out.println("Codigo traducido:\n");
-		System.out.println(cadenasalida);*/
+		System.out.println(cadenasalida);
 		
 		/*String cadenasalida2="";
 		String[] palabras = texto.split("   ");
